8730a7942093f98ab0d4e9462ab6344c37d88175
ggkMilan <guogk@betainfo.cn>
2022-09-20 14:36:26 +0800

    Initial commit

---
+++ src/main/java/com/example/yyyy/YyyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.yyyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yyyy/YyyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yyyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
